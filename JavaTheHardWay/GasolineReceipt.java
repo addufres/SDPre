5020e731429c0e51d17ab2516e605d436da9a14a
AndrewD <adufresplatinum@gmail.com>
2018-02-03 08:59:26 -0700

    GasolineReceipt JTHW

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main(String[] args) {
+    System.out.println("+------------------------+");
+    System.out.println("|                        |");
+    System.out.println("|      CORNER STORE      |");
+    System.out.println("|                        |");
+    System.out.println("| 2018-02-02  08:54AM    |");
+    System.out.println("|                        |");
+    System.out.println("| Gallons:       10.870  |");
+    System.out.println("| Price/gallon: $ 2.910  |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:  $ 31.63   |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}
