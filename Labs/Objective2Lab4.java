dc1e9d86e3ee5eb748e3510a47b64861905e5ede
AndrewD <adufresplatinum@gmail.com>
2018-02-03 10:44:05 -0700

    Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 2;
+    num2 = 66;
+    num3 = 4.223;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
