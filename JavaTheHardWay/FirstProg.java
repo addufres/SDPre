fc1f4f51dc06d6b8a5a9895c951f3522ba6a2bc3
AndrewD <adufresplatinum@gmail.com>
2018-02-03 08:46:17 -0700

    FirstProg JTHW

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+        System.out.println( "I, Andrew Dufresne, am determined to learn how to code." );
+        System.out.println( "Today's date is Saturday, February 2, 2018.");
+    }
+}
