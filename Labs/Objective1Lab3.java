9462d47a9fb952d6d765da531abc9e13871201e0
AndrewD <adufresplatinum@gmail.com>
2018-02-03 08:29:04 -0700

    Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 { //not sure if supposed to be as named or Objective0Lab3
+  public static void main(String[] args) {
+    System.out.println("Name : Andrew Dufresne");
+    System.out.println("Age : 27");
+    System.out.println("State : Oklahoma");
+  }
+}
