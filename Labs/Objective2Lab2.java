4056d9b76826b10393e8903f220fdd8dd95e0fa6
AndrewD <adufresplatinum@gmail.com>
2018-02-03 10:35:13 -0700

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Andrew Dufresne";
+    int age = 27;
+    String state = "Oklahoma";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
